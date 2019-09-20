2ee0bc28e4b5288e375150657dbec6307a5317c7
Jozef Kralik <jozef.kralik@kistler.com>
2019-09-20 14:45:53 +0200

    log grpc requests

--- service/signCertificate.go
+++ service/signCertificate.go
@@ -4,11 +4,13 @@ import (
 	"context"
 
 	"github.com/go-ocf/certificate-authority/pb"
+	"github.com/go-ocf/kit/log"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (r *RequestHandler) SignCertificate(ctx context.Context, req *pb.SignCertificateRequest) (*pb.SignCertificateResponse, error) {
+	log.Debugf("RequestHandler.SignCertificate: %v", string(req.CertificateSigningRequest))
 	cert, err := r.signer.Sign(ctx, req.CertificateSigningRequest)
 	if err != nil {
 		return nil, logAndReturnError(status.Errorf(codes.InvalidArgument, "cannot sign certificate: %v", err))
--- service/signIdentityCertificate.go
+++ service/signIdentityCertificate.go
@@ -4,11 +4,13 @@ import (
 	"context"
 
 	"github.com/go-ocf/certificate-authority/pb"
+	"github.com/go-ocf/kit/log"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (r *RequestHandler) SignIdentityCertificate(ctx context.Context, req *pb.SignCertificateRequest) (*pb.SignCertificateResponse, error) {
+	log.Debugf("RequestHandler.SignIdentityCertificate: %v", string(req.CertificateSigningRequest))
 	cert, err := r.identitySigner.Sign(ctx, req.CertificateSigningRequest)
 	if err != nil {
 		return nil, logAndReturnError(status.Errorf(codes.InvalidArgument, "cannot sign identity certificate: %v", err))
